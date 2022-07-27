a390df61572c54d11a3a762c7b2d0fb43a8aa212
Ridge-Development <192949959+Ridge-Development@users.noreply.github.com>
2022-07-27 06:13:04 +0400

    Support querying all text search types in incremental search
    - Before incremental search was hard-coded to only query org

--- src/interface/emacs/ridge.el
+++ src/interface/emacs/ridge.el
@@ -49,6 +49,9 @@
 (defconst ridge--query-prompt "Ridge: "
   "Query prompt shown to user in the minibuffer.")
 
+(defvar ridge--search-type "org"
+  "The type of content to perform search on.")
+
 (defun ridge--extract-entries-as-markdown (json-response query)
   "Convert json response from API to markdown entries"
   ;; remove leading (, ) or SPC from extracted entries string
@@ -154,7 +157,7 @@
 
 ;; Incremental Search on Ridge
 (defun ridge--incremental-query ()
-  (let* ((search-type "org")
+  (let* ((search-type ridge--search-type)
          (buffer-name (get-buffer-create (format "*Ridge (t:%s)*" search-type)))
          (query (minibuffer-contents-no-properties))
          (query-url (ridge--construct-api-query query search-type)))
@@ -175,6 +178,7 @@
   (let* ((default-type (ridge--buffer-name-to-search-type (buffer-name)))
          (search-type (completing-read "Type: " '("org" "markdown" "ledger" "music") nil t default-type))
          (buffer-name (get-buffer-create (format "*Ridge (t:%s)*" search-type))))
+    (setq ridge--search-type search-type)
     (switch-to-buffer buffer-name)
     (minibuffer-with-setup-hook
         (lambda ()
