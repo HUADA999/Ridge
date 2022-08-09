5bd934940bc382d3aefdfd7b1d9bf7bc5c70f2c2
HUADA999 <192949959+HUADA999@users.noreply.github.com>
2022-08-09 20:52:52 +0300

    By default, wait for 2 seconds before initiating rerank in ridge.el
    - Subjectively, previous default seems to aggressive based on usage
      Doesn't give time for user to think and type their query

--- src/interface/emacs/cleo.el
+++ src/interface/emacs/cleo.el
@@ -46,7 +46,7 @@
   :group 'ridge
   :type 'integer)
 
-(defcustom ridge-rerank-after-idle-time 1.0
+(defcustom ridge-rerank-after-idle-time 2.0
   "Idle time (in seconds) to trigger cross-encoder to rerank incremental search results."
   :group 'ridge
   :type 'float)
