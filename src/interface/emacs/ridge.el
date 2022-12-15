d84e95af51389cce5f111719aa2051e864b7fa95
Ridge-Development <192949959+Ridge-Development@users.noreply.github.com>
2022-12-14 22:47:07 -0300

    Fix Ridge Emacs package URL in ridge.el

--- src/interface/emacs/ridge.el
+++ src/interface/emacs/ridge.el
@@ -7,7 +7,7 @@
 ;; Keywords: search, org-mode, outlines, markdown, beancount, ledger, image
 ;; Version: 0.2.0
 ;; Package-Requires: ((emacs "27.1"))
-;; URL: http://github.com/debanjum/ridge/interface/emacs
+;; URL: https://github.com/debanjum/ridge/tree/master/src/interface/emacs
 
 ;; This file is NOT part of GNU Emacs.
 
