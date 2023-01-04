e602fb47ab22043cf8b44d02fdf40fe9d02fb89f
Ridge-Development <192949959+Ridge-Development@users.noreply.github.com>
2023-01-03 21:36:59 -0300

    Allow starting ridge in non-GUI mode without config file instantiated
    - Start ridge server (in non-GUI mode) without needing config file
      already instantiated.
      - But throw warning to configure ridge to use it
    - This allows plugins to configure the app via the /config/data APIs
    - To be used by the Ridge obsidian plugin to configure markdown content
      in ridge

--- src/configure.py
+++ src/configure.py
@@ -26,9 +26,10 @@ logger = logging.getLogger(__name__)
 def configure_server(args, required=False):
     if args.config is None:
         if required:
-            print('Exiting as Ridge is not configured. Configure the application to use it.')
+            logger.error(f'Exiting as Ridge is not configured.\nConfigure it via GUI or by editing {state.config_file}.')
             sys.exit(1)
         else:
+            logger.warn(f'Ridge is not configured.\nConfigure it via ridge GUI, plugins or by editing {state.config_file}.')
             return
     else:
         state.config = args.config
