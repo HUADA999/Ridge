98ba6cd69ee9ed54ba6d6f936d7f965a325aef9d
HUADA999 <192949959+HUADA999@users.noreply.github.com>
2023-01-19 03:02:59 -0300

    Rename configuration sections in ridge.el transient menu

--- src/interface/emacs/cleo.el
+++ src/interface/emacs/cleo.el
@@ -413,11 +413,11 @@ Render results in BUFFER-NAME."
 
 (transient-define-prefix ridge-menu ()
   "Create Ridge Menu to Configure and Execute Commands."
-  [["Set"
+  [["Configure General"
     ("t" "Content Type" ridge--content-type-switch)]
-   ["Set Search"
+   ["Configure Search"
     ("n" "Results Count" "--results-count=" :init-value (lambda (obj) (oset obj value (format "%s" ridge-results-count))))]
-   ["Set Update"
+   ["Configure Update"
     ("-f" "Force Update" "--force-update")]]
   [["Act"
     ("s" "Search" ridge--search-command)
