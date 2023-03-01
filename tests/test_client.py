805a4beaf382fb30e8e80e84add2c4aaed5fc8af
HUADA999 <192949959+HUADA999@users.noreply.github.com>
2023-03-01 00:35:21 -0600

    Fix configuring search types & /config/types API when no plugin configured
    - Test /config/types API when no plugin configured, only plugin configured
      and no content configured scenarios
    - Do not throw null reference exception while configuring search types
      when no plugin configured
    - Do not throw null reference exception on calling /config/types API
      when no plugin configured
    
    Resolves bug introduced by #173

--- tests/test_client.py
+++ tests/test_client.py
@@ -9,8 +9,9 @@ from fastapi.testclient import TestClient
 
 # Internal Packages
 from ridge.main import app
-from ridge.configure import configure_routes
-from ridge.utils.state import model
+from ridge.configure import configure_routes, configure_search_types
+from ridge.utils import state
+from ridge.utils.state import model, config
 from ridge.search_type import text_search, image_search
 from ridge.utils.rawconfig import ContentConfig, SearchConfig
 from ridge.processor.org_mode.org_to_jsonl import OrgToJsonl
@@ -84,6 +85,59 @@ def test_get_configured_types_via_api(client):
     # Assert
     assert response.status_code == 200
     assert response.json() == ["org", "image", "plugin1"]
+
+
+# ----------------------------------------------------------------------------------------------------
+def test_get_configured_types_with_only_plugin_content_config(content_config):
+    # Arrange
+    config.content_type = ContentConfig()
+    config.content_type.plugins = content_config.plugins
+    state.SearchType = configure_search_types(config)
+
+    configure_routes(app)
+    client = TestClient(app)
+
+    # Act
+    response = client.get(f"/api/config/types")
+
+    # Assert
+    assert response.status_code == 200
+    assert response.json() == ["plugin1"]
+
+
+# ----------------------------------------------------------------------------------------------------
+def test_get_configured_types_with_no_plugin_content_config(content_config):
+    # Arrange
+    config.content_type = content_config
+    config.content_type.plugins = None
+    state.SearchType = configure_search_types(config)
+
+    configure_routes(app)
+    client = TestClient(app)
+
+    # Act
+    response = client.get(f"/api/config/types")
+
+    # Assert
+    assert response.status_code == 200
+    assert "plugin1" not in response.json()
+
+
+# ----------------------------------------------------------------------------------------------------
+def test_get_configured_types_with_no_content_config():
+    # Arrange
+    config.content_type = ContentConfig()
+    state.SearchType = configure_search_types(config)
+
+    configure_routes(app)
+    client = TestClient(app)
+
+    # Act
+    response = client.get(f"/api/config/types")
+
+    # Assert
+    assert response.status_code == 200
+    assert response.json() == []
 
 
 # ----------------------------------------------------------------------------------------------------
