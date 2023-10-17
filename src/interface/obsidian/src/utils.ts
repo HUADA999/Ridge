0f786f001a2bd740e07af9b25b1a824da4f2a9b2
HUADA999 <192949959+HUADA999@users.noreply.github.com>
2023-10-17 04:30:27 -0700

    Update indexer API endpoint URL to index/update from indexer/batch
    New URL follows action oriented endpoint naming convention used for
    other Ridge API endpoints
    
    Update desktop, obsidian and emacs client to call this new API
    endpoint

--- src/interface/obsidian/src/utils.ts
+++ src/interface/obsidian/src/utils.ts
@@ -68,7 +68,7 @@ export async function updateContentIndex(vault: Vault, setting: RidgeSetting, la
     }
 
     // Call Ridge backend to update index with all markdown, pdf files
-    const response = await fetch(`${setting.ridgeUrl}/api/v1/indexer/batch?regenerate=${regenerate}`, {
+    const response = await fetch(`${setting.ridgeUrl}/api/v1/index/update?regenerate=${regenerate}`, {
         method: 'POST',
         headers: {
             'x-api-key': 'secret',
