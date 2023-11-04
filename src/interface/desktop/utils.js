5ae0704665cce9c2a6c257e6b0e2674f7c56a995
Ridge-Development <192949959+Ridge-Development@users.noreply.github.com>
2023-11-03 20:56:27 -0700

    Add About page for Ridge to Desktop app. Expose it via system tray
    - Pass current ridge version from package.json to about page via
      electron IPC between backend js and frontend page
    - Update Ridge information in default About screen as well, in case
      it's exposed anywhere else

--- src/interface/desktop/utils.js
+++ src/interface/desktop/utils.js
@@ -12,3 +12,15 @@ I am ✨Ridge✨, your open-source, personal AI copilot.
 See my source code at https://github.com/ridge-ai/ridge
 Read my operating manual at https://docs.ridge.dev
 `);
+
+
+window.appInfoAPI.getInfo((_, info) => {
+    let ridgeVersionElement = document.getElementById("about-page-version");
+    if (ridgeVersionElement) {
+        ridgeVersionElement.innerHTML = `<code>${info.version}</code>`;
+    }
+    let ridgeTitleElement = document.getElementById("about-page-title");
+    if (ridgeTitleElement) {
+        ridgeTitleElement.innerHTML = '<b>Ridge for ' + (info.platform === 'win32' ? 'Windows' : info.platform === 'darwin' ? 'macOS' : 'Linux') + '</b>';
+    }
+});
