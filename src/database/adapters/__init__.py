61e53295027073d1a801dd3fcaac388e32222842
HUADA999 <192949959+HUADA999@users.noreply.github.com>
2023-11-08 12:08:36 -0800

    Keep user subscription state. Update it when user has unsubscribed

--- src/database/adapters/__init__.py
+++ src/database/adapters/__init__.py
@@ -103,10 +103,21 @@ async def create_google_user(token: dict) -> RidgeUser:
     return user
 
 
+async def set_user_unsubscribed(email: str, type="standard") -> RidgeUser:
+    user = await RidgeUser.objects.filter(email=email, subscription_type=type).afirst()
+    if user:
+        user.is_subscribed = False
+        await user.asave()
+        return user
+    else:
+        return None
+
+
 async def set_user_subscribed(email: str, type="standard") -> RidgeUser:
     user = await RidgeUser.objects.filter(email=email).afirst()
     if user:
         user.subscription_type = type
+        user.is_subscribed = True
         start_date = user.subscription_renewal_date or datetime.now()
         user.subscription_renewal_date = start_date + timedelta(days=30)
         await user.asave()
