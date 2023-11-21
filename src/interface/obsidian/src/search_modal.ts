b4cb269bdb13b42ff270dafe9342bf1a83ee4d22
HUADA999 <192949959+HUADA999@users.noreply.github.com>
2023-11-21 11:19:33 -0800

    Search across all content types from Ridge Obsidian client
    Previously it was only searching for PDF and Markdown files. This was
    meant to show only content from current vault as results.
    
    But it has not scaled well as other clients also allow syncing PDF and
    markdown files now. So remove this content type filter for now.
    
    A proper solution would limit by using file/dir filters on server or
    client side.

--- src/interface/obsidian/src/search_modal.ts
+++ src/interface/obsidian/src/search_modal.ts
@@ -87,27 +87,18 @@ export class RidgeSearchModal extends SuggestModal<SearchResult> {
     }
 
     async getSuggestions(query: string): Promise<SearchResult[]> {
-        // Query Ridge backend for search results
+        // Setup Query Ridge backend for search results
         let encodedQuery = encodeURIComponent(query);
         let searchUrl = `${this.setting.ridgeUrl}/api/search?q=${encodedQuery}&n=${this.setting.resultsCount}&r=${this.rerank}&client=obsidian`;
         let headers = { 'Authorization': `Bearer ${this.setting.ridgeApiKey}` }
 
-        // Get search results for markdown and pdf files
-        let mdResponse = await request({ url: `${searchUrl}&t=markdown`, headers: headers });
-        let pdfResponse = await request({ url: `${searchUrl}&t=pdf`, headers: headers });
+        // Get search results from Ridge backend
+        let response = await request({ url: `${searchUrl}`, headers: headers });
 
         // Parse search results
-        let mdData = JSON.parse(mdResponse)
+        let results = JSON.parse(response)
             .filter((result: any) => !this.find_similar_notes || !result.additional.file.endsWith(this.app.workspace.getActiveFile()?.path))
-            .map((result: any) => { return { entry: result.entry, score: result.score, file: result.additional.file }; });
-        let pdfData = JSON.parse(pdfResponse)
-            .filter((result: any) => !this.find_similar_notes || !result.additional.file.endsWith(this.app.workspace.getActiveFile()?.path))
-            .map((result: any) => { return { entry: `## ${result.additional.compiled}`, score: result.score, file: result.additional.file } as SearchResult; })
-
-        // Combine markdown and PDF results and sort them by score
-        let results = mdData.concat(pdfData)
-            .sort((a: any, b: any) => a.score - b.score)
-            .map((result: any) => { return { entry: result.entry, file: result.file } as SearchResult; })
+            .map((result: any) => { return { entry: result.entry, file: result.additional.file } as SearchResult; });
 
         this.query = query;
         return results;
