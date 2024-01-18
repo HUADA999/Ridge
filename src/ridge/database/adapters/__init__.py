4ab1fe4712c3659d6b818cb8de61c6c06ff4d323
Ridge-Development <192949959+Ridge-Development@users.noreply.github.com>
2024-01-18 05:54:14 -0800

    Add support for a first-party client app to call into Ridge (Part 1)
    * Add support for a first party client app
    - Based on a client id and client secret, allow a first party app to call into the Ridge backend with a phone number identifier
    - Add migration to add phone numbers to the RidgeUser object
    * Add plus in front of country code when registering a phone number.
    - Decrease free tier limit to 5 (from 10)
    - Return a response object when handling stripe webhooks
    * Fix telemetry method which references authenticated user's client app
    * Add better error handling for null phone numbers, simplify logic of authenticating user
    * Pull the client_secret in the API call from the authorization header
    * Add a migration merge to resolve phone number and other changes

--- src/ridge/database/adapters/__init__.py
+++ src/ridge/database/adapters/__init__.py
@@ -17,6 +17,7 @@ from torch import Tensor
 
 from ridge.database.models import (
     ChatModelOptions,
+    ClientApplication,
     Conversation,
     Entry,
     GithubConfig,
@@ -40,7 +41,7 @@ from ridge.search_filter.file_filter import FileFilter
 from ridge.search_filter.word_filter import WordFilter
 from ridge.utils import state
 from ridge.utils.config import GPT4AllProcessorModel
-from ridge.utils.helpers import generate_random_name
+from ridge.utils.helpers import generate_random_name, is_none_or_empty
 
 
 class SubscriptionState(Enum):
@@ -82,6 +83,28 @@ async def get_or_create_user(token: dict) -> RidgeUser:
     user = await get_user_by_token(token)
     if not user:
         user = await create_user_by_google_token(token)
+    return user
+
+
+async def aget_or_create_user_by_phone_number(phone_number: str) -> RidgeUser:
+    if is_none_or_empty(phone_number):
+        return None
+    user = await aget_user_by_phone_number(phone_number)
+    if not user:
+        user = await acreate_user_by_phone_number(phone_number)
+    return user
+
+
+async def acreate_user_by_phone_number(phone_number: str) -> RidgeUser:
+    if is_none_or_empty(phone_number):
+        return None
+    user, _ = await RidgeUser.objects.filter(phone_number=phone_number).aupdate_or_create(
+        defaults={"username": phone_number, "phone_number": phone_number}
+    )
+    await user.asave()
+
+    await Subscription.objects.acreate(user=user, type="trial")
+
     return user
 
 
@@ -187,6 +210,12 @@ async def get_user_by_token(token: dict) -> RidgeUser:
     return google_user.user
 
 
+async def aget_user_by_phone_number(phone_number: str) -> RidgeUser:
+    if is_none_or_empty(phone_number):
+        return None
+    return await RidgeUser.objects.filter(phone_number=phone_number).prefetch_related("subscription").afirst()
+
+
 async def retrieve_user(session_id: str) -> RidgeUser:
     session = SessionStore(session_key=session_id)
     if not await sync_to_async(session.exists)(session_key=session_id):
@@ -270,6 +299,12 @@ async def aset_user_search_model(user: RidgeUser, search_model_config_id: int):
     return new_config
 
 
+class ClientApplicationAdapters:
+    @staticmethod
+    async def aget_client_application_by_id(client_id: str, client_secret: str):
+        return await ClientApplication.objects.filter(client_id=client_id, client_secret=client_secret).afirst()
+
+
 class ConversationAdapters:
     @staticmethod
     def get_conversation_by_user(user: RidgeUser):
@@ -279,11 +314,11 @@ class ConversationAdapters:
         return Conversation.objects.create(user=user)
 
     @staticmethod
-    async def aget_conversation_by_user(user: RidgeUser):
-        conversation = Conversation.objects.filter(user=user)
+    async def aget_conversation_by_user(user: RidgeUser, client_application: ClientApplication = None):
+        conversation = Conversation.objects.filter(user=user, client=client_application)
         if await conversation.aexists():
             return await conversation.afirst()
-        return await Conversation.objects.acreate(user=user)
+        return await Conversation.objects.acreate(user=user, client=client_application)
 
     @staticmethod
     async def adelete_conversation_by_user(user: RidgeUser):
