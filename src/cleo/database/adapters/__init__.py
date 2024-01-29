6fafc9e2415f55e47d85315973b7adf558c92a14
HUADA999 <192949959+HUADA999@users.noreply.github.com>
2024-01-29 01:57:06 -0800

    Store rate limiter-related metadata in the database for more resilience
    * Store rate limiter-related metadata in the database for more resilience
    - This helps maintain state even between server restarts
    - Allows you to scale up workers on your service without having to implement sticky routing
    * Make the usage exceeded message less abrasive
    * Fix rate limiter for specific conversation commands and improve the copy

--- src/cleo/database/adapters/__init__.py
+++ src/cleo/database/adapters/__init__.py
@@ -34,6 +34,7 @@ from ridge.database.models import (
     Subscription,
     TextToImageModelConfig,
     UserConversationConfig,
+    UserRequests,
     UserSearchModelConfig,
 )
 from ridge.search_filter.date_filter import DateFilter
@@ -282,6 +283,10 @@ def get_user_github_config(user: RidgeUser):
 def get_user_notion_config(user: RidgeUser):
     config = NotionConfig.objects.filter(user=user).first()
     return config
+
+
+def delete_user_requests(window: timedelta = timedelta(days=1)):
+    return UserRequests.objects.filter(created_at__lte=datetime.now(tz=timezone.utc) - window).delete()
 
 
 async def set_text_content_config(user: RidgeUser, object: Type[models.Model], updated_config):
