0071ceec63b3fbade2838344ecade121912a1bf9
HUADA999 <192949959+HUADA999@users.noreply.github.com>
2024-01-30 18:18:55 -0800

    Fix subscription state detection for users based on phone numbers, emails
    * Fix subscription state detection for users based on phone numbers, emails
    * Fix unit tests for api_user4
    * Use a single method for determining subscription from user
    * Pass user object, rather than user.email for getting subscription state

--- src/cleo/configure.py
+++ src/cleo/configure.py
@@ -160,7 +160,7 @@ class UserAuthenticationBackend(AuthenticationBackend):
             if subscribed:
                 return (
                     AuthCredentials(["authenticated", "premium"]),
-                    AuthenticatedRidgeUser(user),
+                    AuthenticatedRidgeUser(user, client_application),
                 )
             return AuthCredentials(["authenticated"]), AuthenticatedRidgeUser(user, client_application)
         if state.anonymous_mode:
