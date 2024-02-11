8a2ee120052f5e2dbcff116af18e713cf4ed7196
HUADA999 <192949959+HUADA999@users.noreply.github.com>
2024-02-11 02:18:28 -0800

    Support multiple chat sessions within the web UI
    * Enable support for multiple chat sessions within the web client
    
    - Allow users to create multiple chat sessions and manage them
    - Give chat session slugs based on the most recent message
    - Update web UI to have a collapsible menu with active chats
    - Move chat routes into a separate file
    
    * Make the collapsible side panel more graceful, improve some styling elements of the new layout
    
    * Support modification of the conversation title
    
    - Add a new field to the conversation object
    - Update UI to add a threedotmenu to each conversation
    
    * Get the default conversation if a matching one is not found by id

--- src/cleo/configure.py
+++ src/cleo/configure.py
@@ -256,6 +256,7 @@ def initialize_content(regenerate: bool, search_type: Optional[SearchType] = Non
 def configure_routes(app):
     # Import APIs here to setup search types before while configuring server
     from ridge.routers.api import api
+    from ridge.routers.api_chat import api_chat
     from ridge.routers.api_config import api_config
     from ridge.routers.auth import auth_router
     from ridge.routers.indexer import indexer
@@ -266,6 +267,7 @@ def configure_routes(app):
     app.include_router(indexer, prefix="/api/v1/index")
     app.include_router(web_client)
     app.include_router(auth_router, prefix="/auth")
+    app.include_router(api_chat, prefix="/api/chat")
 
     if state.billing_enabled:
         from ridge.routers.subscription import subscription_router
