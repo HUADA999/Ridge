7e1c153525ff173387c2ce30143cf9dbe553a8ac
Ridge-Development <192949959+Ridge-Development@users.noreply.github.com>
2024-05-27 10:42:09 +0800

    fix: emacs tests

--- src/interface/emacs/tests/ridge-tests.el
+++ src/interface/emacs/tests/ridge-tests.el
@@ -219,7 +219,7 @@ Rule everything\n")
         (progn
           (should
            (equal
-            (ridge--render-files-as-request-body (list upgrade-file act-file) '() "ridge")
+            (ridge--render-update-files-as-request-body (list upgrade-file act-file) "ridge")
             (format
             "\n--ridge\r\n\
 Content-Disposition: form-data; name=\"files\"; filename=\"%s\"\r\n\
@@ -244,7 +244,7 @@ Rule everything\n\n\r\n\
         (progn
           (should
            (equal
-            (ridge--render-files-as-request-body (list upgrade-file act-file) (list upgrade-file act-file "/tmp/deleted-file.org") "ridge")
+            (ridge--render-update-files-as-request-body (list upgrade-file act-file "/tmp/deleted-file.org") "ridge")
             (format
             "\n--ridge\r\n\
 Content-Disposition: form-data; name=\"files\"; filename=\"%s\"\r\n\
