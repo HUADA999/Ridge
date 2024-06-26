7fa283d2507393ef00bd8704a8c83de383ea42b0
HUADA999 <192949959+HUADA999@users.noreply.github.com>
2024-06-26 18:10:43 +0530

    Put cursor on chat input when focus on chat view in Obsidian
    This should improve fluidity of keyboard interactions with Ridge on
    Obsidian.
    
    Open Ridge chat view via keybinding or command pallete and ask
    question using only the keyboard, with no mouse clicks required

--- src/interface/obsidian/src/main.ts
+++ src/interface/obsidian/src/main.ts
@@ -88,7 +88,16 @@ export default class Ridge extends Plugin {
             await leaf?.setViewState({ type: viewType, active: true });
         }
 
-        // "Reveal" the leaf in case it is in a collapsed sidebar
-        if (leaf) workspace.revealLeaf(leaf);
+        if (leaf) {
+            if (viewType === RidgeView.CHAT) {
+                // focus on the chat input when the chat view is opened
+                let chatView = leaf.view as RidgeChatView;
+                let chatInput = <HTMLTextAreaElement>chatView.contentEl.getElementsByClassName("ridge-chat-input")[0];
+                if (chatInput) chatInput.focus();
+            }
+
+            // "Reveal" the leaf in case it is in a collapsed sidebar
+            workspace.revealLeaf(leaf);
+        }
     }
 }
