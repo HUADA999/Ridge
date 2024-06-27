93861d1e1776546c78a57e75b45c497ec81f8edf
Ridge-Development <192949959+Ridge-Development@users.noreply.github.com>
2024-06-27 01:26:16 -0700

    Add our first view via Next.js for Agents
    Initialize our migration to use Next.js for front-end views via Agents. This includes setup for getting authenticated users, reading in available agents, setting up a pop-up modal when you're clicking on an agent, and allowing users to start new conversations with agents.
    
    Best attempt at an in-place migration, though there are some noticeable differences.
    
    Also adds view for chat that are not being used, but in experimental phase.

---
+++ src/interface/web/app/chat/layout.tsx
@@ -0,0 +1,24 @@
+import type { Metadata } from "next";
+import { Noto_Sans } from "next/font/google";
+import "../globals.css";
+
+const inter = Noto_Sans({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Ridge AI - Chat",
+  description: "Use this page to chat with Ridge AI.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        {children}
+      </body>
+    </html>
+  );
+}
