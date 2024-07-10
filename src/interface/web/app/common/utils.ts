f83bfe5397eba4390cbfb45d023612c1b54097a8
HUADA999 <192949959+HUADA999@users.noreply.github.com>
2024-07-10 10:03:08 +0530

    Add DOMPurify for rendering md text. Add a easter egg in the console

---
+++ src/interface/web/app/common/utils.ts
@@ -0,0 +1,17 @@
+export function welcomeConsole() {
+    console.log(`%c %s`, "font-family:monospace", `
+        __  __     __  __     ______       __        _____      __
+       /\\ \\/ /    /\\ \\_\\ \\   /\\  __ \\     /\\ \\      /\\  __ \\   /\\ \\
+       \\ \\  _"-.  \\ \\  __ \\  \\ \\ \\/\\ \\   _\\_\\ \\     \\ \\  __ \\  \\ \\ \\
+        \\ \\_\\ \\_\\  \\ \\_\\ \\_\\  \\ \\_____\\ /\\_____\\     \\ \\_\\ \\_\\  \\ \\_\\
+         \\/_/\\/_/   \\/_/\\/_/   \\/_____/ \\/_____/      \\/_/\\/_/   \\/_/
+
+
+       Greetings traveller,
+
+       I am ✨Ridge✨, your open-source, personal AI copilot.
+
+       See my source code at https://github.com/ridge-ai/ridge
+       Read my operating manual at https://docs.ridge.dev
+       `);
+}
