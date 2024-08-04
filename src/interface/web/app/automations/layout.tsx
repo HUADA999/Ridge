c2c57bdbbee872291047800bc8a79c8f0d0b7c41
Ridge-Development <192949959+Ridge-Development@users.noreply.github.com>
2024-08-05 02:27:31 +0530

    Format next.js web app with prettier

--- src/interface/web/app/automations/layout.tsx
+++ src/interface/web/app/automations/layout.tsx
@@ -3,12 +3,11 @@ import { Toaster } from "@/components/ui/toaster";
 
 import "../globals.css";
 
-
 export const metadata: Metadata = {
     title: "Ridge AI - Automations",
     description: "Use Autoomations with Ridge to simplify the process of running repetitive tasks.",
     icons: {
-        icon: '/static/favicon.ico',
+        icon: "/static/favicon.ico",
     },
 };
 
--- src/interface/web/app/share/chat/layout.tsx
+++ src/interface/web/app/share/chat/layout.tsx
@@ -5,29 +5,29 @@ import "../../globals.css";
 const inter = Noto_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Ridge AI - Chat",
-  description: "Use this page to view a chat with Ridge AI.",
+    title: "Ridge AI - Chat",
+    description: "Use this page to view a chat with Ridge AI.",
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <meta httpEquiv="Content-Security-Policy"
-        content="default-src 'self' https://assets.ridge.dev;
+    return (
+        <html lang="en">
+            <meta
+                httpEquiv="Content-Security-Policy"
+                content="default-src 'self' https://assets.ridge.dev;
                        script-src 'self' https://assets.ridge.dev 'unsafe-inline' 'unsafe-eval';
                        connect-src 'self' https://ipapi.co/json ws://localhost:42110;
                        style-src 'self' https://assets.ridge.dev 'unsafe-inline' https://fonts.googleapis.com;
                        img-src 'self' data: https://*.ridge.dev https://*.googleusercontent.com https://*.google.com/ https://*.gstatic.com;
                        font-src 'self' https://assets.ridge.dev https://fonts.gstatic.com;
                        child-src 'none';
-                       object-src 'none';"></meta>
-      <body className={inter.className}>
-        {children}
-      </body>
-    </html>
-  );
+                       object-src 'none';"
+            ></meta>
+            <body className={inter.className}>{children}</body>
+        </html>
+    );
 }
