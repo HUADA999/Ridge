23d0ff6fc4491417ad3d754c7a9e578f43e1fe02
HUADA999 <192949959+HUADA999@users.noreply.github.com>
2024-11-25 22:06:05 -0800

    Create first version of Ridge Android app from PWA using Bubblewrap

---
+++ src/interface/android/app/src/main/java/dev/cleo/app/DelegationService.java
@@ -0,0 +1,17 @@
+package dev.ridge.app;
+
+
+import com.google.androidbrowserhelper.locationdelegation.LocationDelegationExtraCommandHandler;
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+
+            registerExtraCommandHandler(new LocationDelegationExtraCommandHandler());
+
+    }
+}
