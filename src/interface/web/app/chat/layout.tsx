a7a2b6ee6676344cf4f7607d64c3275471ad3abf
Ridge-Development <192949959+Ridge-Development@users.noreply.github.com>
2024-12-08 17:57:27 -0800

    Centralize definition of the content security policy and add in-app chat
    - in-app chat is meant for support requests and currently is only in the settings page, where users are most likely to go if confused IMO

--- src/interface/web/app/chat/layout.tsx
+++ src/interface/web/app/chat/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { noto_sans, noto_sans_arabic } from "@/app/fonts";
 import "../globals.css";
+import { ContentSecurityPolicy } from "../common/layoutHelper";
 
 export const metadata: Metadata = {
     title: "Ridge AI - Chat",
@@ -33,18 +34,7 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en" className={`${noto_sans.variable} ${noto_sans_arabic.variable}`}>
-            <meta
-                httpEquiv="Content-Security-Policy"
-                content="default-src 'self' https://assets.ridge.dev;
-                       media-src * blob:;
-                       script-src 'self' https://assets.ridge.dev 'unsafe-inline' 'unsafe-eval';
-                       connect-src 'self' blob: https://ipapi.co/json ws://localhost:42110;
-                       style-src 'self' https://assets.ridge.dev 'unsafe-inline' https://fonts.googleapis.com;
-                       img-src 'self' data: blob: https://*.ridge.dev https://*.googleusercontent.com https://*.google.com/ https://*.gstatic.com;
-                       font-src 'self' https://assets.ridge.dev https://fonts.gstatic.com;
-                       child-src 'none';
-                       object-src 'none';"
-            ></meta>
+            <ContentSecurityPolicy />
             <body>
                 {children}
                 <script
