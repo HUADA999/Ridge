129dd5d9e3bb0f38d49dd69bae54b1cb8c74ffe0
Ridge-Development <192949959+Ridge-Development@users.noreply.github.com>
2024-12-15 13:54:41 -0800

    Include email in verification API

--- src/ridge/database/adapters/__init__.py
+++ src/ridge/database/adapters/__init__.py
@@ -269,8 +269,8 @@ async def astart_trial_subscription(user: RidgeUser) -> Subscription:
     return subscription
 
 
-async def aget_user_validated_by_email_verification_code(code: str) -> RidgeUser:
-    user = await RidgeUser.objects.filter(email_verification_code=code).afirst()
+async def aget_user_validated_by_email_verification_code(code: str, email: str) -> RidgeUser:
+    user = await RidgeUser.objects.filter(email_verification_code=code, email=email).afirst()
     if not user:
         return None
 
