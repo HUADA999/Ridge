HUADA999/Ridge

commit 631b93262ec3975f3b4f85e5c132cb68648052b3

Decouple Django CSRF, ALLOWED_HOST settings for more complex setups
- Set RIDGE_ALLOWED_DOMAIN to the domain that Ridge is accessible on
  from the host machine. This can be the internal i.p or domain of the
  host machine.

  It can be used by your load balancer/reverse_proxy to access Ridge.
  For example, if the load balancer service is in the ridge docker
  network, RIDGE_DOMAIN will be `server' (i.e service name).

- Set RIDGE_DOMAIN to your externally accessible DOMAIN or I.P to avoid
  CSRF trusted origin or unset cookie issue when trying to access the
  ridge admin panel.

Resolves #1114

src/cleo/app/settings.py
@@ -32,7 +32,8 @@ DEBUG = in_debug_mode()
 
 # All Subdomains of RIDGE_DOMAIN are trusted
 RIDGE_DOMAIN = os.getenv("RIDGE_DOMAIN", "ridge.dev")
-ALLOWED_HOSTS = [f".{RIDGE_DOMAIN}", "localhost", "127.0.0.1", "[::1]", f"{RIDGE_DOMAIN}"]
+RIDGE_ALLOWED_DOMAIN = os.getenv("RIDGE_ALLOWED_DOMAIN", RIDGE_DOMAIN)
+ALLOWED_HOSTS = [f".{RIDGE_ALLOWED_DOMAIN}", "localhost", "127.0.0.1", "[::1]", f"{RIDGE_ALLOWED_DOMAIN}"]
 
 CSRF_TRUSTED_ORIGINS = [
     f"https://*.{RIDGE_DOMAIN}",
@@ -45,7 +46,7 @@ CSRF_TRUSTED_ORIGINS = [
 DISABLE_HTTPS = is_env_var_true("RIDGE_NO_HTTPS")
 
 COOKIE_SAMESITE = "None"
-if DEBUG or os.getenv("RIDGE_DOMAIN") == None:
+if DEBUG and os.getenv("RIDGE_DOMAIN") == None:
     SESSION_COOKIE_DOMAIN = "localhost"
     CSRF_COOKIE_DOMAIN = "localhost"
 else:

docker-compose.yml
@@ -81,12 +81,18 @@ services:
       # - OLOSTEP_API_KEY=your_olostep_api_key
       #
       # Uncomment the necessary lines below to make your instance publicly accessible.
-      # Replace the RIDGE_DOMAIN with either your domain or IP address (no http/https prefix).
       # Proceed with caution, especially if you are using anonymous mode.
       # ---
       # - RIDGE_NO_HTTPS=True
+      # Replace the RIDGE_DOMAIN with the server's externally accessible domain or I.P address from a remote machie (no http/https prefix).
+      # Ensure this is set correctly to avoid CSRF trusted origin or unset cookie issue when trying to access the admin panel.
       # - RIDGE_DOMAIN=192.168.0.104
       # - RIDGE_DOMAIN=ridge.example.com
+      # Replace the RIDGE_ALLOWED_DOMAIN with the server's internally accessible domain or I.P address on the host machine (no http/https prefix).
+      # Only set if using a load balancer/reverse_proxy in front of your Ridge server. If unset, it defaults to RIDGE_DOMAIN.
+      # For example, if the load balancer service is added to the ridge docker network, set RIDGE_ALLOWED_DOMAIN to ridge's docker service name: `server'.
+      # - RIDGE_ALLOWED_DOMAIN=server
+      # - RIDGE_ALLOWED_DOMAIN=127.0.0.1
       # Uncomment the line below to disable telemetry.
       # Telemetry helps us prioritize feature development and understand how people are using Ridge
       # Read more at https://docs.ridge.dev/miscellaneous/telemetry

documentation/docs/get-started/setup.mdx
@@ -283,9 +283,14 @@ Go to http://localhost:42110/server/admin and login with the admin credentials y
 Ensure you are using **localhost, not 127.0.0.1**, to access the admin panel to avoid the CSRF error.
 :::
 
+:::info[CSRF Trusted Origin or Unset Cookie Error]
+If using a load balancer/reverse_proxy in front of your Ridge server: Set the environment variable RIDGE_ALLOWED_DOMAIN=your-internal-ip-or-domain to avoid this error.
+If unset, it defaults to RIDGE_DOMAIN.
+:::
+
 :::info[DISALLOWED HOST or Bad Request (400) Error]
 You may hit this if you try access Ridge exposed on a custom domain (e.g. 192.168.12.3 or example.com) or over HTTP.
-Set the environment variables RIDGE_DOMAIN=your-domain and RIDGE_NO_HTTPS=True if required to avoid this error.
+Set the environment variables RIDGE_DOMAIN=your-external-ip-or-domain and RIDGE_NO_HTTPS=True if required to avoid this error.
 :::
 
 :::tip[Note]

commit 0cc8d9eb3fac420750f6c793630ca4562e8a6c13

Fix login to local admin panel without need to set RIDGE_DEBUG
Issue introduced in commit 631b932.

Usage of RIDGE_DOMAIN
---
RIDGE_DOMAIN is tri-state for local, official and other production deployments:
- If RIDGE_DOMAIN is unset (for local):
  - sets CSRF cookie to localhost
  - adds ridge.dev variants to ALLOWED_HOSTS, CSRF_TRUSTED_ORIGINS
  - adds app.ridge.dev variants to CORS origins
- If RIDGE_DOMAIN is set to empty (for official):
  - sets CSRF to ridge.dev
  - adds ridge.dev variants to ALLOWED_HOSTS, CSRF_TRUSTED_ORIGINS
  - adds app.ridge.dev variants to CORS origins
- If RIDGE_DOMAIN is set (for other prod deployments):
  - sets CSRF cookie to RIDGE_DOMAIN
  - adds RIDGE_DOMAIN variants to ALLOWED_HOSTS, CSRF_TRUSTED_ORIGINS
  - adds RIDGE_DOMAIN variants to CORS origins

Related #1137, #1152

Resolves #1123

src/cleo/app/settings.py
@@ -17,7 +17,7 @@ from pathlib import Path
 
 from django.templatetags.static import static
 
-from ridge.utils.helpers import in_debug_mode, is_env_var_true
+from ridge.utils.helpers import is_env_var_true
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -29,11 +29,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("RIDGE_DJANGO_SECRET_KEY", "!secret")
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = in_debug_mode()
-
 # All Subdomains of RIDGE_DOMAIN are trusted
-RIDGE_DOMAIN = os.getenv("RIDGE_DOMAIN", "ridge.dev")
+RIDGE_DOMAIN = os.getenv("RIDGE_DOMAIN") or "ridge.dev"
 RIDGE_ALLOWED_DOMAIN = os.getenv("RIDGE_ALLOWED_DOMAIN", RIDGE_DOMAIN)
 ALLOWED_HOSTS = [f".{RIDGE_ALLOWED_DOMAIN}", "localhost", "127.0.0.1", "[::1]", f"{RIDGE_ALLOWED_DOMAIN}"]
 
@@ -47,8 +44,12 @@ CSRF_TRUSTED_ORIGINS = [
 
 DISABLE_HTTPS = is_env_var_true("RIDGE_NO_HTTPS")
 
-COOKIE_SAMESITE = "None"
-if DEBUG and os.getenv("RIDGE_DOMAIN") == None:
+# RIDGE_DOMAIN is tri-state.
+# - Unset it for local deployments.
+# - Set it to empty for official production deployment.
+# - Set it to custom domain for other production deployments.
+# WARNING: Change this check only if you know what you are doing.
+if os.getenv("RIDGE_DOMAIN") == None:
     SESSION_COOKIE_DOMAIN = "localhost"
     CSRF_COOKIE_DOMAIN = "localhost"
 else:
@@ -58,6 +59,7 @@ else:
     if not DISABLE_HTTPS:
         SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 
+COOKIE_SAMESITE = "None"
 if DISABLE_HTTPS:
     SESSION_COOKIE_SECURE = False
     CSRF_COOKIE_SECURE = False

src/cleo/main.py
@@ -66,7 +66,7 @@ else:
 django_app = get_asgi_application()
 
 # Add CORS middleware
-RIDGE_DOMAIN = os.getenv("RIDGE_DOMAIN", "app.ridge.dev")
+RIDGE_DOMAIN = os.getenv("RIDGE_DOMAIN") or "app.ridge.dev"
 scheme = "https" if not is_env_var_true("RIDGE_NO_HTTPS") else "http"
 custom_origins = [f"{scheme}://{RIDGE_DOMAIN.strip()}", f"{scheme}://{RIDGE_DOMAIN.strip()}:*"]
 default_origins = [